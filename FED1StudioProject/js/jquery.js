24b28b82353c26c25c2380d8cb0716c68f1374e6
BryanClingan <BryanClingan@outlook.com>
2021-01-14 22:46:27 -0600

    added tables added datatables  added styling.

--- FED1StudioProject/js/jquery.js
+++ FED1StudioProject/js/jquery.js
@@ -1,21 +1,29 @@
 ﻿///<reference path="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" />
 $(function () {
     $(".thumb").on("click", function () {
-        //Find the source attribute of the img tag that was clicked and store that as a variable
+        
         var imgSrc = $(this).attr("src");
 
-        //Plug an <img> into the #lightbox-content
+        
         $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive"/>');
 
 
-        //Making the lightbox visible by fading in
-        $("#lightbox-container").fadeIn(500);
+       
+        $("#lightbox-container").fadeIn(1000);
 
-        //when the user clicks anywhere in the light box container, fadeOut()
+        
         $("#lightbox-container").on("click", function () {
-            $(this).fadeOut(500);
+            $(this).fadeOut(1000);
 
         });
 
 });
+
+$('.classTable1').DataTable({
+
+        'paging':false,
+        'searching':false,
+
+});
+
 });
